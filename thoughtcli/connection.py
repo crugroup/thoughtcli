d782c51b32c585275fdc9764ae2417f5477d1955
Jeremy Dang <jeremy.dang@crugroup.com>
2026-06-17 11:05:20 +0100

    pin version and remove redundant features

--- thoughtcli/connection.py
+++ thoughtcli/connection.py
@@ -12,38 +12,6 @@ class TSProfile:
 
 
 class TSConnection:
-    def __init__(self, profile: TSProfile):
-        self.profile = profile
-        self.user_pass_auth = profile.username and profile.password
-
-
-class V2Connection(TSConnection):
-    def __init__(self, profile: TSProfile):
-        super().__init__(profile)
-        self.client = TSRestApiV2(server_url=profile.server_url)
-
-    def __enter__(self):
-        if self.user_pass_auth:
-            self.client.auth_session_login(
-                username=self.profile.username,
-                password=self.profile.password,
-                org_identifier=self.profile.org_identifier,
-            )
-        else:
-            resp = self.client.auth_token_full(
-                username=self.profile.username,
-                secret_key=self.profile.secret_key,
-                org_id=self.profile.org_identifier,
-            )
-            self.client.bearer_token = resp["token"]
-
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.client.auth_session_logout()
-
-
-class TSConnection:
     def __init__(self, profile: TSProfile, metadata_max_size: int = 1000):
         self.metadata_max_size = metadata_max_size
         self.v2 = V2Connection(profile)
